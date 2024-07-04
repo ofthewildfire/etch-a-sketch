cf37413db4171ae616eba505c838baf350b719df
ofthewildfire <103148299+ofthewildfire@users.noreply.github.com>
2024-07-04 10:37:10 +0200

    Project complete: Excuse me, but, holy hell, I did it? This was cool and I actually did it.

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
-const container = document.getElementById("app");
-let arrayOfDivs = [];
+let container = document.getElementById("app");
 
-// Loop and create a div for each.
-for (let index = 0; index < 256; index++) {
-  arrayOfDivs.push(`<div class="grid"></div>`);
+const colorsArr = [
+  "#d188d1",
+  "#cae22c",
+  "#324cd0",
+  "#9cd188",
+  "#e116c9",
+  "#83ea2f",
+];
+
+for (let i = 0; i < 256; i++) {
+  const div = document.createElement("div");
+  div.setAttribute("class", "grid");
+  div.addEventListener("mouseenter", (e) => {
+    let randomIndex = Math.floor(Math.random() * colorsArr.length);
+    e.target.style.backgroundColor = colorsArr[randomIndex];
+  });
+  container.appendChild(div);
 }
-
-// Ahh, this made me happy. It works
-arrayOfDivs.forEach((el) => {
-  container.innerHTML += el;
-});
-
-container.addEventListener("mouseenter", (e) => {
-  console.log(e.target);
-});
